24a31dfca88ff3e556805ac6066bc381bfa1776d
Ahnaf-dev <52428475+Ahnaf-dev@users.noreply.github.com>
2020-02-10 10:47:50 -0700

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -5,7 +5,15 @@
 USE SchoolTranscript
 GO
 
-INSERT INTO Students(StudentID, GivenName, Surname, DateOfBirth) -- notice no Enrolled column
-VALUES (123456, 'Ahnaf','19720614 10:30:09 AM')
+INSERT INTO Students(GivenName, Surname, DateOfBirth) -- notice no Enrolled column
+VALUES ('Ahnaf', 'bari' ,'19720614 10:30:09 AM'),
+       ('Charles' , 'Kuhn', '199006 00:00:00 AM'),
+       ('Penny', 'Harrison', '19971104 00:00:00 AM')
+
 
 SELECT * FROM Students
+
+INSERT INTO StudentCourses(StudentID,CourseNumber,[Year],Term,FinalMark,[Status])
+VALUES ('14766314', 'A05', '2020', '1' , '89', 'Finished'),
+       ('147763840', 'A08', '2020', '2', '80', 'Finished'),
+       ('14776310', 'A08', '2020', '2', '80', 'Finished')
